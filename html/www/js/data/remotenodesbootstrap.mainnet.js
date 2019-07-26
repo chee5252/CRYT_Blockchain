ac9aac0b57463eec125224639d384fecefa62580
Jacky <43884431+CryTrExcom@users.noreply.github.com>
2019-07-26 22:46:04 +0200

    Add files via upload

--- html/www/js/data/remotenodesbootstrap.mainnet.js
+++ html/www/js/data/remotenodesbootstrap.mainnet.js
@@ -17,6 +17,131 @@
 //TODO: manually add bootstrap nodes
 RemoteNodesManager.prototype.REMOTE_NODES_BOOTSTRAP =
 {
-    "peers": [
+    "peers": [{
+    "downloadedVolume": 16434424,
+    "address": "185.221.172.127",
+    "inbound": true,
+    "blockchainState": "UP_TO_DATE",
+    "weight": 0,
+    "uploadedVolume": 16433761,
+    "services": [
+        "API",
+        "CORS"
+    ],
+    "requestProcessingTime": 1,
+    "version": "1.11.15",
+    "platform": "Linux amd64",
+    "inboundWebSocket": true,
+    "lastUpdated": 63894068,
+    "blacklisted": false,
+    "announcedAddress": "185.221.172.127",
+    "apiPort": 11112,
+    "application": "CryTrExCoin",
+    "port": 11111,
+    "outboundWebSocket": true,
+    "lastConnectAttempt": 63894068,
+    "state": 1,
+    "shareAddress": true
+},
+{
+    "downloadedVolume": 18555071,
+    "address": "159.203.191.59",
+    "inbound": true,
+    "blockchainState": "UP_TO_DATE",
+    "weight": 0,
+    "uploadedVolume": 16436264,
+    "services": [
+        "API",
+        "CORS"
+    ],
+    "requestProcessingTime": 0,
+    "version": "1.11.15",
+    "platform": "Linux amd64",
+    "inboundWebSocket": true,
+    "lastUpdated": 63894008,
+    "blacklisted": false,
+    "announcedAddress": "159.203.191.59",
+    "apiPort": 11112,
+    "application": "CryTrExCoin",
+    "port": 11111,
+    "outboundWebSocket": true,
+    "lastConnectAttempt": 63894008,
+    "state": 1,
+    "shareAddress": true
+},
+{
+    "downloadedVolume": 6956094,
+    "address": "134.209.60.44",
+    "inbound": false,
+    "blockchainState": "UP_TO_DATE",
+    "weight": 0,
+    "uploadedVolume": 13795700,
+    "services": [
+        "API",
+        "CORS"
+    ],
+    "requestProcessingTime": 0,
+    "version": "1.11.15",
+    "platform": "Linux amd64",
+    "inboundWebSocket": false,
+    "lastUpdated": 63894128,
+    "blacklisted": false,
+    "announcedAddress": "134.209.60.44",
+    "apiPort": 11112,
+    "application": "CryTrExCoin",
+    "port": 11111,
+    "outboundWebSocket": true,
+    "lastConnectAttempt": 63894128,
+    "state": 1,
+    "shareAddress": true
+},
+{
+    "downloadedVolume": 17922907,
+    "address": "134.209.156.107",
+    "inbound": true,
+    "blockchainState": "UP_TO_DATE",
+    "weight": 0,
+    "uploadedVolume": 16403333,
+    "services": [
+        "API",
+        "CORS"
+    ],
+    "requestProcessingTime": 0,
+    "version": "1.11.15",
+    "platform": "Linux amd64",
+    "inboundWebSocket": true,
+    "lastUpdated": 63894438,
+    "blacklisted": false,
+    "announcedAddress": "134.209.156.107",
+    "apiPort": 11112,
+    "application": "CryTrExCoin",
+    "port": 11111,
+    "outboundWebSocket": true,
+    "lastConnectAttempt": 63894438,
+    "state": 1,
+    "shareAddress": true
+},
+{
+    "downloadedVolume": 19221213,
+    "address": "68.183.37.174",
+    "inbound": true,
+    "blockchainState": "UP_TO_DATE",
+    "weight": 0,
+    "uploadedVolume": 17490914,
+    "services": [],
+    "requestProcessingTime": 0,
+    "version": "1.11.15",
+    "platform": "Linux amd64",
+    "inboundWebSocket": false,
+    "lastUpdated": 63894028,
+    "blacklisted": false,
+    "announcedAddress": "68.183.37.174",
+    "application": "CryTrExCoin",
+    "port": 11111,
+    "outboundWebSocket": true,
+    "lastConnectAttempt": 63894028,
+    "state": 1,
+    "shareAddress": true
+}
     ]
 };
